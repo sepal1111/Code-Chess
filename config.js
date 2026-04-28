ddca1773ebff08156bc98305d7ac72e437ca4d67
sepal1111 <59186181+sepal1111@users.noreply.github.com>
2026-04-28 14:51:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 老師們只需修改此檔案，無需重新安裝開發環境或編譯
 window.WAR_CHESS_CONFIG = {
   // 請在此處填入你部署好的 Google Apps Script 網頁應用程式網址
-  APPS_SCRIPT_URL: "Google Apps Script 網頁應用程式網址"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzr7aIzMRnxcxZ_8p6460mZfaPMRitMsEKGpEFy1wLUnwV-mwVrpn8gjVrvIFzC8yZW8Q/exec"
 };
